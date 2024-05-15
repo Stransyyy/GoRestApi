98f62a683bc7a50c2e88660d3470cb3a3eb627c9
Alan Cano <alancorreonuevo5@gmail.com>
2024-05-15 11:17:33 -0500

    changes

--- api.go
+++ api.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 type APIserver struct {
 	listenAddr string
 }
@@ -8,4 +10,27 @@ func NewAPIserver(listenAddr string) *APIserver {
 	return &APIserver{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *APIserver) HandleAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+
+func (s *APIserver) HandleGetAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+
+func (s *APIserver) HandleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+func (s *APIserver) HandleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+func (s *APIserver) HandleTransaction(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,3 @@
 module github.com/Stransyyy/GoRestApi
 
 go 1.21.6
-
-require github.com/sirupsen/logrus v1.9.3
-
-require (
-	github.com/go-chi/chi v1.5.5
-	github.com/gorilla/schema v1.3.0
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
-)
